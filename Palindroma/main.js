01112a78634a7b0a51ccc976be7ba00fbb218b4f
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-10-29 14:56:37 +0200

    add JS-palindroma

---
+++ Palindroma/main.js
@@ -0,0 +1,34 @@
+/* 
+    PALINDROMA
+    Chiedere all’utente di inserire una parola
+    Creare una funzione per capire se la parola inserita è palindroma 
+*/
+let word = '';
+do{
+    word = prompt('inserisci una parola');
+
+} while(!isNaN(word));
+console.log('Parola:', word);
+
+const palindromeWord = reverseWord(word);
+
+
+/**************
+  FUNZIONI
+ **************/
+function reverseWord(palind){ 
+    
+    let reverse = '';
+
+    for(let i = palind.length - 1; i >= 0; i--){
+         reverse += palind[i];
+    }
+    console.log('parola invertita:', reverse);
+
+    if(palind === reverse){
+        console.log("La parola E' palindroma")
+    } else{
+        console.log("La parola NON E' palindroma")
+    }
+    
+}
